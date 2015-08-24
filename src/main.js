5fd1d38f9a058b3bc8842ac6182da6b44de19e89
shotaro.akiyama <satetsu888@gmail.com>
2015-08-25 00:19:04 +0900

    Add person sample

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ var Vue = require('vue')
 new Vue({
   el: '#app',
   components: {
-    'color-component': require('./color.vue')
+    'color-component': require('./color.vue'),
+    'person-component': require('./person.vue')
   }
 });
---
+++ src/person.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>name:<input type=text v-model="name"></p>
+        <p>nickname:<input type=text v-model="nickname"></p>
+        <p>age:<input type=range min=0 max=200 v-model="age">{{age}}</p>
+
+        changed: {{changed}} <br>
+        <input type=button value="Reset" v-on="click: reset">
+        <input type=button value="Update" v-on="click: setAsBase()">
+    </div>
+</template>
+
+<script>
+var resettable = require('vue-resettable');
+module.exports = {
+    props: ["name", "nickname", "age", "gender"],
+    mixins: [resettable]
+};
+</script>
