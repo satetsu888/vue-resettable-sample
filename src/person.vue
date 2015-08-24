aed7a7845d10dad5e5666a83b276c07fe1543ac5
shotaro.akiyama <satetsu888@gmail.com>
2015-08-25 08:24:25 +0900

    fix something at person.vue

--- src/person.vue
+++ src/person.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <p>name:<input type=text v-model="name"></p>
-        <p>nickname:<input type=text v-model="nickname"></p>
-        <p>age:<input type=range min=0 max=200 v-model="age">{{age}}</p>
+        <p>name: <input type=text v-model="name"></p>
+        <p>nickname: <input type=text v-model="nickname"></p>
+        <p>age: <input type=range min=0 max=200 v-model="age">{{age}}</p>
 
         changed: {{changed}} <br>
         <input type=button value="Reset" v-on="click: reset">
@@ -13,7 +13,7 @@
 <script>
 var resettable = require('vue-resettable');
 module.exports = {
-    props: ["name", "nickname", "age", "gender"],
+    props: ["name", "nickname", "age" ],
     mixins: [resettable]
 };
 </script>
